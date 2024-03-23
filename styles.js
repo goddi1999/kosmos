28c5c1d6149dfc32c1b7832a7324735cb20a2b80
goddi1999 <godfredasefa@icloud.com>
2024-03-23 16:31:41 +0100

    initial commit

---
+++ styles.js
@@ -0,0 +1,4 @@
+import { StyleSheet } from 'react-native';
+import { colors, values } from './variables';
+
+export default StyleSheet.create({});
---
+++ variables.js
@@ -0,0 +1,54 @@
+const colors = {
+    white: '#FFFFFF', // white
+    black: '#000000', // black
+    transparent: 'transparent', // transparent
+};
+
+const values = {
+    buttonPadding: '',
+    inputPadding: '',
+    containerPadding: '',
+    containerPaddingXL: '',
+    containerPaddingY: '',
+    duration75: '75ms',
+    duration100: '100ms',
+    duration150: '150ms',
+    duration200: '200ms',
+    duration300: '300ms',
+    duration750: '750ms',
+    duration1: '1000ms',
+    rounded: 15,
+    roundedSm: 25,
+    roundedMd: 25,
+    roundedLg: 50,
+    roundedXl: 20,
+    roundedXxl: 35,
+    roundedFull: 9999,
+    flexBoxGapXxs: Math.round(0.2 * 10),
+    flexBoxGapXs: Math.round(0.8 * 10),
+    flexBoxGapMd: Math.round(2.0 * 10),
+    flexBoxGapLg: Math.round(3.5 * 10),
+    flexBoxGapXl: Math.round(4 * 10),
+    flexBoxGapXxl: Math.round(6.5 * 10),
+    fontSizeDisplaySm: 36,
+    fontSizeDisplayMd: 42,
+    fontSizeDisplayLg: 54,
+    fontSizeDisplayXl: 66,
+    fontSizeDisplayXxl: 126,
+    fontSizeXxs: 6,
+    fontSizeXs: 9,
+    fontSizeSm: 10.5,
+    fontSizeMd: 12,
+    fontSizeLg: 13,
+    fontSizeXl: 15,
+    fontSizeXxl: 18,
+    mgXxs: 5,
+    mgXs: 10,
+    mgSm: 15,
+    mgMd: 20,
+    mgLg: 30,
+    mgXl: 40,
+    mgXxl: 100,
+};
+
+export { colors, values };
